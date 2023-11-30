ec5cc5eae213996ac8fc59b7f6c1c2bf641749b1
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2023-11-30 22:24:50 +0100

    esercizio caricato

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> student = new ArrayList<String>();
+        student.add(String.valueOf(new Student("Fabio", 21)));
+        student.add(String.valueOf(new Student("Giulia", 20)));
+        student.add(String.valueOf(new Student("Mario", 23)));
+        student.add(String.valueOf(new Student("Franco", 21)));
+        student.add(String.valueOf(new Student("Giorgia", 23)));
+        student.add(String.valueOf(new Student("Marco", 25)));
+        student.add(String.valueOf(new Student("Luca", 20)));
+        student.add(String.valueOf(new Student("Simona", 21)));
+        student.add(String.valueOf(new Student("Chiara", 22)));
+        student.add(String.valueOf(new Student("Elisabetta", 20)));
+        student.add(String.valueOf(new Student("Margherita", 23)));
+        student.add(String.valueOf(new Student("Simone", 22)));
+        System.out.println(student);
+        System.out.println(student);
+
+        Collections.sort(student);
+        for (String i : student) {
+            System.out.println(i);
+        }
+
+    }
+}
